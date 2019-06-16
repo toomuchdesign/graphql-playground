b69daa5a24b8336fcb06ad839afed09e74b76370
Andrea Carraro <me@andreacarraro.it>
2019-06-16 14:10:03 +0200

    Setup graphql with mocks

--- server-ql/index.js
+++ server-ql/index.js
@@ -2,31 +2,94 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+let messages = [{
+  id: '0',
+  content: 'initial value',
+  author: 'Mark',
+}];
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
+  type Message {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+
+  input MessageInput {
+    content: String!,
+    author: String!,
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  random: () => {
-    return Math.random();
-  },
-  rollDice: function ({numDice, numSides}) {
+// This class implements the RandomDie GraphQL type
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
     var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
     }
     return output;
   }
-};
+}
+
+// The root provides the top-level API endpoints
+var root = {
+  getDie: function ({numSides}) {
+    return new RandomDie(numSides || 6);
+  },
+  getMessage: ({id}) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(messages.filter(message => message.id === id)[0])
+      }, 2000)
+    });
+  },
+  createMessage: ({input}) => {
+    const id = String(messages.length);
+    messages.push({
+      id,
+      ...input,
+    });
+    return root.getMessage({id});
+  },
+  updateMessage: ({id, input}) => {
+    messages = messages.map(message => {
+      if(message.id === id){
+        return {
+          ...message,
+          ...input,
+        }
+      }
+      return message;
+    })
+    return root.getMessage({id});
+  },
+}
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
